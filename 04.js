14464c8da061af60d84b03ba039ae8c32fb0a9ce
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-04 10:51:07 +0000

    Update 04.js

--- 04.js
+++ 04.js
@@ -12,3 +12,18 @@ q04a=(x)=>{
     })
     return count
 }
+
+q04b=(x)=>{
+    let count = 0
+    x.split("\n").forEach(y=>{
+        pairs = y.split(",")
+        pair1 = pairs[0].split("-")
+        pair2 = pairs[1].split("-")
+        a = parseInt(pair1[0])
+        b = parseInt(pair1[1])
+        c = parseInt(pair2[0])
+        d = parseInt(pair2[1])
+        if ((a >= c && a <=d )||(b >= c && b <= d)||(a <= c && b >=d)) count++
+    })
+    return count
+}
