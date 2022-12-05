7fd49a7c420cd97bdd199218fcfd8497a35a21b2
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-05 12:37:30 +0000

    Update 05.js

--- 05.js
+++ 05.js
@@ -31,5 +31,40 @@ q05a=(x)=>{
     return topLetters
 }
 q05b=(x)=>{
-    return 0
+    topLetters= ''
+    setup = true
+    firstsetup = true
+    stackcount = 0
+    x.split("\n").forEach(x=>{
+        if (x=="") {
+            setup = false
+        } else {
+            if (setup) {
+                for (y=4;y<=x.length+1;y+=4){
+                    chunk = x.substring(y-4,y)
+                    if (firstsetup) {
+                        window['stack'+y/4] = []
+                        stackcount++
+                    }
+                    if (!(chunk[1]==" ")) window['stack'+y/4].unshift(chunk[1])
+                }
+            firstsetup = false
+            } else {
+                line = x.split(" ")
+                movestack=[]
+                for (numberoftimes=line[1];numberoftimes>0;numberoftimes--){
+                    movestack.push(window['stack'+line[3]].pop())
+                }
+                for (numberoftimes=line[1];numberoftimes>0;numberoftimes--){
+                    window['stack'+line[5]].push(movestack.pop())
+                }
+
+            }
+        }
+    })
+    for(c=1;c<=stackcount;c++){
+        topLetters += window['stack'+c].pop()
+    }
+    return topLetters
 }
+
