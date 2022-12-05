1196416606e3813f87fabdf1ea439b68a297d8a4
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-05 19:28:08 +0000

    Update 04.js

--- 04.js
+++ 04.js
@@ -1,6 +1,6 @@
-q04a=(x)=>{
+q04=(data,rules=0)=>{
     let count = 0
-    x.split("\n").forEach(y=>{
+    data.split("\n").forEach(y=>{
         pairs = y.split(",")
         pair1 = pairs[0].split("-")
         pair2 = pairs[1].split("-")
@@ -8,22 +8,15 @@ q04a=(x)=>{
         b = parseInt(pair1[1])
         c = parseInt(pair2[0])
         d = parseInt(pair2[1])
-        if ((a>=c)&&(b<=d)||(c>=a)&&(d<=b)) count++
+        switch (rules){
+            case 1:
+                if ((a >= c && a <=d )||(b >= c && b <= d)||(a <= c && b >=d)||(c <=a && d >= b)) count++
+                break;
+            default:
+                if ((a>=c && b<=d)||(c>=a && d<=b)) count++
+        }
     })
     return count
 }
-
-q04b=(x)=>{
-    let count = 0
-    x.split("\n").forEach(y=>{
-        pairs = y.split(",")
-        pair1 = pairs[0].split("-")
-        pair2 = pairs[1].split("-")
-        a = parseInt(pair1[0])
-        b = parseInt(pair1[1])
-        c = parseInt(pair2[0])
-        d = parseInt(pair2[1])
-        if ((a >= c && a <=d )||(b >= c && b <= d)||(a <= c && b >=d)||(c <=a && d >= b)) count++
-    })
-    return count
-}
+q04a=(x)=>{ return q04(x) }
+q04b=(x)=>{ return q04(x,1) }
