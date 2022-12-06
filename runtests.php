91433953257a22e5595705a6fbe649201e941438
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-06 19:40:03 +0000

    Update runtests.php

--- runtests.php
+++ runtests.php
@@ -12,17 +12,25 @@
     <script src="./tests.js"></script>
     </head>
     <body><pre>
-    <ul id='results'></ul></pre>
+    <ul id='results'></ul><button onclick="runtests(tests)">Re-Run Tests</button>
     <script>
-        test=(func,data,result)=>{
-            return func(data) == result
-        }
-        tests.forEach(x=>{
-            status = (test(x[1],x[2],x[3])) ?  "Passed" :  "Failed"
-            a = document.createElement('li')
-            a.innerHTML = x[0]+' -- '+status
-            document.getElementById('results').appendChild(a)
+    test=(func,data,result)=> {
+       return func(data)==result
+    }
+    (runtests=(t)=>{
+        document.getElementById('results').innerHTML=""
+        t.map(x=>{
+            if (x[3]) {
+                let t0= performance.now()
+                status=test(x[1],x[2],x[3])?"Passed":"Failed"
+                let t1= performance.now()
+                exectime=(t1-t0).toFixed(10) 
+                a=document.createElement('li')
+                a.innerHTML=x[0]+' -- '+status+' in '+exectime+' milliseconds'
+                document.getElementById('results').appendChild(a)
+            }
         })
+    })(tests)
     </script>
     </body>
 </html>
