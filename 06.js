29c91d4edf07634368546a59d0bb6e4ba453c7f5
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-06 09:25:13 +0000

    Update 06.js
    b not yet working

--- 06.js
+++ 06.js
@@ -10,5 +10,22 @@ q06a=(x)=>{
     return markerfollows
 }
 q06b=(x)=>{
-    return 0
+    markerfollows = uniquecount= 0
+    chunk = []
+    b = x.split("")
+    for (c=0;c<b.length;c++){
+        if (uniquecount==14){
+            markerfollows = c
+            console.log(markerfollows)
+        }
+        if(chunk.includes(b[c])){
+            uniquecount = 0
+        }else{
+            uniquecount++
+        }
+        if (chunk.length=14) chunk.pop()
+        chunk.unshift(b[c])
+        console.log(chunk,b[c],uniquecount,markerfollows)
+    }
+    return markerfollows  //not working
 }
