d9779bdf10b6cb02fa9810ccd2d5977adf748aa2
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-07 20:50:11 +0000

    Update 07.js

--- 07.js
+++ 07.js
@@ -1,36 +1,25 @@
 getSize=x=>{
     let size=0
-    if (Number.isInteger(x[1])) {
-        size += x[1]
-    } else {
-        x[1].forEach(y=>{
-            size += getSize(y)
-        })
-    }
+    if (Number.isInteger(x[1])) size+=x[1]
+    else x[1].forEach(y=>size+=getSize(y))
     return size
 }
 
 getSumofSmall=(x,max)=>{
     let sumsize=0
     if (!Number.isInteger(x[1])) {
-        let s = getSize(x)
-        if (s<=max) sumsize +=s
-        x[1].forEach(y=>{
-            sumsize += getSumofSmall(y,max)
-        })
+        let s=getSize(x)
+        if(s<=max)sumsize+=s
+        x[1].forEach(y=>sumsize+=getSumofSmall(y,max))
     }
     return sumsize
 }
 
 getSmallestabove=(x, min, max)=>{
     if (!Number.isInteger(x[1])) {
-        let s = getSize(x)
-        if (s>=min&&s<max) {
-            max = s
-        }
-        x[1].forEach(y=>{
-            max = getSmallestabove(y,min,max)
-        })
+        let s=getSize(x)
+        if(s>=min&&s<max)max=s
+        x[1].forEach(y=>max=getSmallestabove(y,min,max))
     }
     return max
 }
@@ -38,28 +27,25 @@ getSmallestabove=(x, min, max)=>{
 buildfs=(data)=>{
     let commands=data.split("\n")
     depth=0
-    filesystem = ""
+    filesystem=""
     for (let c=0;c<commands.length;c++){
-        line = commands[c].split(" ")
-        if (line[0]=="$"){
-            if (line[1]=="cd"){
-                if (line[2]==".."){
-                    filesystem += "]],"
+        line=commands[c].split(" ")
+        if(line[0]=="$"){
+            if(line[1]=="cd"){
+                if(line[2]==".."){
+                    filesystem+="]],"
                     depth--
-                } else {
+                }else{
                     depth++
-                    filesystem += "['"+line[2]+"',["
+                    filesystem+="['"+line[2]+"',["
                 }
             }
-        } else if (!(line[0]=="dir")){
-            filesystem += "['"+line[1]+"',"+line[0]+"],"
-        }
+        } else if(!(line[0]=="dir"))filesystem+="['"+line[1]+"',"+line[0]+"],"
     }
-    for(;depth>0;depth--){
-        filesystem+="]]"
-    }
-    return eval(filesystem) // I'm sorry.
+    for(;depth>0;depth--)filesystem+="]]"
+    return eval(filesystem)
 }
+
 
 q07a=(x)=>{ return getSumofSmall(buildfs(x),100000) }
 
