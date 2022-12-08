07ee496804ea63dde4e18e63e60baf5fb4a992f8
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-08 19:29:33 +0000

    Update 08.js

--- 08.js
+++ 08.js
@@ -13,35 +13,27 @@ buildForest=data=>{
     return forest
 }
 
-isVisible=(forest,x,y)=>{
-    visible = 15
+visibility=(forest,x,y)=>{
+    score = 15
     len=forest.length
     x++;x--;y++;y--
-    for(let c=y-1;c>=0;c--){
-        if (forest[y][x]<=forest[c][x]){
-            visible -= 1
+    for(let c=y-1;c>=0;c--) if(forest[y][x]<=forest[c][x]){
+            score -= 1
             break;
         }
-    }
-    for(let c=x-1;c>=0;c--){
-        if (forest[y][x]<=forest[y][c]){
-            visible -= 2
+    for(let c=x-1;c>=0;c--) if(forest[y][x]<=forest[y][c]){
+            score -= 2
             break;
         }
-    }
-    for(let c=y+1;c<len;c++){
-        if (forest[y][x]<=forest[c][x]){
-            visible -= 4
+    for(let c=y+1;c<len;c++) if(forest[y][x]<=forest[c][x]){
+            score -= 4
             break;
         }
-    }
-    for(let c=x+1;c<len;c++){
-        if (forest[y][x]<=forest[y][c]){
-            visible -= 8
+    for(let c=x+1;c<len;c++) if(forest[y][x]<=forest[y][c]){
+            score -= 8
             break;
         }
-    }
-    return visible>0
+    return score
 }
 
 scenicScore=(forest,x,y)=>{
@@ -50,54 +42,43 @@ scenicScore=(forest,x,y)=>{
     x++;x--;y++;y--
     if (y==0||x==0||y==len-1||x==len-1) return 0
     else {
-        for(let c=y-1;c>=0;c--){
-            if (c==0||forest[y][x]<=forest[c][x]){
+        for(let c=y-1;c>=0;c--) if(c==0||forest[y][x]<=forest[c][x]){
                 t = y-c
                 break;
             }
-        }
-        for(let c=x-1;c>=0;c--){
-            if (c==0||forest[y][x]<=forest[y][c]){
+        for(let c=x-1;c>=0;c--) if(c==0||forest[y][x]<=forest[y][c]){
                 l = x-c
                 break;
             }
-        }
-        for(let c=y+1;c<len;c++){
-            if (c==len-1||forest[y][x]<=forest[c][x]){
+        for(let c=y+1;c<len;c++) if(c==len-1||forest[y][x]<=forest[c][x]){
                 b = c-y
                 break;
             }
-        }
-        for(let c=x+1;c<len;c++){
-            if (c==len-1||forest[y][x]<=forest[y][c]){
+        for(let c=x+1;c<len;c++) if(c==len-1||forest[y][x]<=forest[y][c]){
                 r = c-x
                 break;
             }
-        }
     }
     return t*l*b*r
 }
 
 q08a=(x)=>{
-    let visable = 0
+    let visible = 0
     forest = buildForest(x)
     len=forest.length
-    for(let c=0;c<len;c++){
-        for(let d=0;d<len;d++){
-            if (isVisible(forest,c,d)) visable++
+    for(let c=0;c<len;c++) for(let d=0;d<len;d++){
+            if (visibility(forest,c,d)>0) visible++
         }
-    }
-    return visable
+    return visible
 }
+
 q08b=(x)=>{
     let bestscore = 0
     forest = buildForest(x)
     len=forest.length
-    for(let c=0;c<len;c++){
-        for(let d=0;d<len;d++){
+    for(let c=0;c<len;c++) for(let d=0;d<len;d++){
             score = scenicScore(forest,c,d)
             if(score>bestscore) bestscore=score
         }
-    }
     return bestscore
 }
