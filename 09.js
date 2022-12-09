e28062054718a9e8572aca475169e0f465a17314
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-09 21:28:34 +0000

    Update 09.js
    works for test data, but not for full data :/

--- 09.js
+++ 09.js
@@ -1,37 +1,49 @@
-move=(direction,number)=>{
-    for(c=1;c<=number;c++){
-        if(direction=="U") headx++
-        if(direction=="D") headx--
-        if(direction=="L") heady--
-        if(direction=="R") heady++
-        relx = headx-tailx
-        rely = heady-taily
-        if(relx==2&&rely==1) {taily++;tailx++}
-        if(relx==2&&rely==-1) {taily--;tailx++}
-        if(relx==-2&&rely==1) {taily++;tailx--}
-        if(relx==-2&&rely==-1) {taily--;tailx--}
-        if(relx==1&&rely==2) {taily++;tailx++}
-        if(relx==-1&&rely==2) {taily++;tailx--}
-        if(relx==1&&rely==-2) {taily--;tailx++}
-        if(relx==-1&&rely==-2) {taily--;tailx--}
-        if(relx==0&&rely==2) taily++
-        if(relx==0&&rely==-2) taily--
-        if(relx==2&&rely==0) tailx++
-        if(relx==-2&&rely==0) tailx--
-        if (!(tailhistory.includes(tailx+","+taily))) tailhistory.push(tailx+","+taily)
+moveSnake=(direction,number,length)=>{
+    for(let c=1;c<=number;c++){
+        if(direction=="U") snake[0][0]++
+        if(direction=="D") snake[0][0]--
+        if(direction=="L") snake[0][1]--
+        if(direction=="R") snake[0][1]++
+        for(let d=1;d<length;d++){
+            let relx = snake[(d-1)][0]-snake[d][0]
+            let rely = snake[(d-1)][1]-snake[d][1]
+            if(relx==2 &&rely==1)  {snake[d][1]++;snake[d][0]++}
+            if(relx==2 &&rely==-1) {snake[d][1]--;snake[d][0]++}
+            if(relx==-2&&rely==1)  {snake[d][1]++;snake[d][0]--}
+            if(relx==-2&&rely==-1) {snake[d][1]--;snake[d][0]--}
+            if(relx==1 &&rely==2)  {snake[d][1]++;snake[d][0]++}
+            if(relx==-1&&rely==2)  {snake[d][1]++;snake[d][0]--}
+            if(relx==1 &&rely==-2) {snake[d][1]--;snake[d][0]++}
+            if(relx==-1&&rely==-2) {snake[d][1]--;snake[d][0]--}
+            if(relx==0 &&rely==2)   snake[d][1]++;
+            if(relx==0 &&rely==-2)  snake[d][1]--;
+            if(relx==2 &&rely==0)   snake[d][0]++;
+            if(relx==-2&&rely==0)   snake[d][0]--;
+            if(relx==2&&rely==2)   {snake[d][0]++;snake[d][1]++;}
+            if(relx==2&&rely==-2)  {snake[d][0]--;snake[d][1]++;}
+            if(relx==-2&&rely==2)  {snake[d][0]++;snake[d][1]--;}
+            if(relx==-2&&rely==-2) {snake[d][0]--;snake[d][1]--;}
+        }
+        if (!(snaketailhistory.includes(snake[length-1][0]+","+snake[length-1][1]))) snaketailhistory.push(snake[length-1][0]+","+snake[length-1][1])
     }
 }
 
-q09a=(x)=>{
-    headx=heady=tailx=taily=0
-    tailhistory=[]
+buildSnake=(length)=>{
+    snake=[]
+    for(c=0;c<length;c++) snake.push([0,0])
+    return snake
+}
+
+q09=(x,length)=>{
+    snake=buildSnake(length)
+    snaketailhistory=[]
     x.split("\n").forEach(y=>{
         d=y.split(" ")[0]
         n=y.split(" ")[1]
-        move(d,n)
+        moveSnake(d,n,snake.length)
     })
-    return tailhistory.length
+    return snaketailhistory.length
 }
-q09b=(x)=>{
-    return 0
-}
+
+q09a=x=>{ return q09(x,2) }
+q09b=x=>{ return q09(x,10) }
