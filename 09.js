e51620a83cf9a76978ca9fe722bf11f05c52b198
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-10 16:07:49 +0000

    Update 09.js

--- 09.js
+++ 09.js
@@ -4,29 +4,27 @@ moveMap = [[[-1,-1],[-1,-1],[-1,0],[-1,1],[-1,1]],
            [[ 1,-1],[ 0, 0],[ 0,0],[ 0,0],[ 1,1]],
            [[ 1,-1],[ 1,-1],[ 1,0],[ 1,1],[ 1,1]]]
 
-moveSnake=(direction,number,len)=>{
-    for(let c=1;c<=number;c++){
-        if(direction=="U") snake[0][0]++
-        if(direction=="D") snake[0][0]--
-        if(direction=="L") snake[0][1]--
-        if(direction=="R") snake[0][1]++
-        for(let d=1;d<len;d++){
-            let relx = (snake[d-1][0]-snake[d][0])
-            let rely = (snake[d-1][1]-snake[d][1])
-            snake[d][0]+=moveMap[relx+2][rely+2][0]
-            snake[d][1]+=moveMap[relx+2][rely+2][1]
-        }
-        if (!(snaketailhistory.includes(snake[len-1][0]+","+snake[len-1][1]))){
-            snaketailhistory.push(snake[len-1][0]+","+snake[len-1][1])
-        } 
-    }
-}
-
 q09=(x,length)=>{
     snake=[]
     snaketailhistory=[]
     for(c=0;c<length;c++) snake.push([0,0])
-    x.split("\n").forEach(y=>{ moveSnake(y.split(" ")[0],y.split(" ")[1],snake.length) })
+    x.split("\n").forEach(y=>{
+        move=y.split(" ")
+        for(let c=1;c<=move[1];c++){
+            if(move[0]=="U") snake[0][0]++
+            if(move[0]=="D") snake[0][0]--
+            if(move[0]=="L") snake[0][1]--
+            if(move[0]=="R") snake[0][1]++
+            for(let d=1;d<snake.length;d++){
+                let relx = (snake[d-1][0]-snake[d][0])
+                let rely = (snake[d-1][1]-snake[d][1])
+                snake[d][0]+=moveMap[relx+2][rely+2][0]
+                snake[d][1]+=moveMap[relx+2][rely+2][1]
+            }
+            loc = snake[snake.length-1][0]+","+snake[snake.length-1][1]
+            if (!(snaketailhistory.includes(loc))) snaketailhistory.push(loc)
+        }
+    })
     return snaketailhistory.length
 }
 
