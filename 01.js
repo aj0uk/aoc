44dc32b026f0fdacb9a89340df481e0ff1aa4976
aj0uk <119697063+aj0uk@users.noreply.github.com>
2022-12-21 14:32:06 +0000

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,3 +1,10 @@
+/*
+Day 1 q1 (test data) -- Passed in 0.10 ms
+Day 1 q1 (full data) -- Passed in 0.60 ms
+Day 1 q2 (test data) -- Passed in 0.00 ms
+Day 1 q2 (full data) -- Passed in 0.50 ms
+*/
+
 q01=(data,topThree=false)=>{
     runningSum=highestSum=currentValue=0
     sums=[]
