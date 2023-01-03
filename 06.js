73ceffdea6a61ed6b6d4be3e0ad31c1a487ffedb
aj0uk <119697063+aj0uk@users.noreply.github.com>
2023-01-03 12:46:50 +0000

    Update 06.js

--- 06.js
+++ 06.js
@@ -4,26 +4,17 @@ Day 6 q1 (full data) -- Passed in 1.10 ms
 Day 6 q2 (test data) -- Passed in 0.10 ms
 Day 6 q2 (full data) -- Passed in 4.70 ms
 */
-allUnique=a=>{
-    matchcount=0
-    len=a.length
-    for (let c=0;c<len;c++){
-        for(let d=0;d<len;d++){
-            if(a[c]==a[d]) matchcount++
-        }
-    }
-    return matchcount == len
+
+allUnique=(a,b=0)=>{
+    for(let c=0;c<a.length;c++)for(let d=0;d<a.length;d++)if(a[c]==a[d]) b++
+    return b==a.length
 }
-q06=(data,uniqueLength)=>{
-    marker=0
-    b=data.split("")
-    for(c=uniqueLength;c<=b.length;c++){
-        if (allUnique(data.substring(c-uniqueLength,c))) {
-            marker=c
-            break
-        }
-    }
-    return marker
-}
-q06a=(x)=>{ return q06(x,4) }
-q06b=(x)=>{ return q06(x,14) }
+
+q06=([data,l])=>{ for(let c=l;c<=data.length;c++)if(allUnique(data.substring(c-l,c))) return c }
+
+q6Tests=[
+    ['Day 06 q1 (test data)',q06,[q06test,4],7],
+    ['Day 06 q1',            q06,[q06data,4],1707],
+    ['Day 06 q2 (test data)',q06,[q06test,14],19],
+    ['Day 06 q2',            q06,[q06data,14],3697]
+]
