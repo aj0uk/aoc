e69962c067bb267e98a9b470dce9ed364a036249
aj0uk <119697063+aj0uk@users.noreply.github.com>
2023-01-03 12:43:01 +0000

    Update 02.js

--- 02.js
+++ 02.js
@@ -5,25 +5,23 @@ Day 2 q2 (test data) -- Passed in 0.00 ms
 Day 2 q2 (full data) -- Passed in 1.00 ms
 */
 
-
-q2gameScore = {
+map02 = {
     X:{A:4,B:1,C:7},
     Y:{A:8,B:5,C:2},
     Z:{A:3,B:9,C:6}
 }
-q2move = {
+
+map03 = {
     X:{A:"Z",B:"X",C:"Y"},
     Y:{A:"X",B:"Y",C:"Z"},
     Z:{A:"Y",B:"Z",C:"X"}
 }
 
-q02=(data,altmove=false)=>{
-    let score = 0
-    data.split("\n").forEach(x=>{
-        moves = x.split(" ")
-        score += q2gameScore[(altmove)?q2move[moves[1]][moves[0]]:moves[1]][moves[0]]
-    })
-    return score
-}
-q02a=(x)=>{ return q02(x) }
-q02b=(x)=>{ return q02(x,true) }
+q02=([a,b,c=0])=> a.split("\n").reduce((s,v)=> s+b[(c)?c[v.split(" ")[1]][v.split(" ")[0]]:v.split(" ")[1]][v.split(" ")[0]] ,0)
+
+q2Tests=[
+    ['Day 02 q1 (test data)',q02,[q02test,map02,0],15],
+    ['Day 02 q1',            q02,[q02data,map02],14264],
+    ['Day 02 q2 (test data)',q02,[q02test,map02,map03],12],
+    ['Day 02 q2',            q02,[q02data,map02,map03],12382]
+]
